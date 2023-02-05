040eb716e81f57ced4e807a102e2daf2d467e01b
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-02-05 20:10:56 +0300

    Help-execution done

--- programming1/5lab/App.java
+++ programming1/5lab/App.java
@@ -4,8 +4,11 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import src.commands.Command;
+import src.commands.Help;
 import src.data.classes.Address;
 import src.data.classes.Coordinates;
 import src.data.classes.Location;
@@ -15,10 +18,15 @@ import src.fillers.Increment;
 
 class App {
     public static void main(String[] args) throws IOException {
-
+        //Execution of "help" command 
+        ArrayList<Command> listOfCommands = new ArrayList<Command>();
+        Help helpCommand = new Help("help", "Output info about all commands", listOfCommands);
+        System.out.println(helpCommand.execute());
+        
+        //Testing unique ID technolody 
         Increment startInc = new Increment(1);
-        Organisation org1 = new Organisation(startInc, "ЛДПР", new Coordinates(1, 1), 2.8, "Пенсионные лидеры", OrganisationType.GOVERNMENT, new Address("ул. Ленина", new Location(1.0, 1F, "Санкт-Петербург")));
-        Organisation org2 = new Organisation(startInc, "ЛДПР", new Coordinates(1, 1), 2.8, "Пенсионные лидеры", OrganisationType.GOVERNMENT, new Address("ул. Ленина", new Location(1.0, 1F, "Санкт-Петербург")));
+        Organisation org1 = new Organisation(startInc, "ЛДПР", new Coordinates(1, 1), 2.8, "LDPR", OrganisationType.GOVERNMENT, new Address("ул. Ленина", new Location(1.0, 1F, "Санкт-Петербург")));
+        Organisation org2 = new Organisation(startInc, "ЛДПР", new Coordinates(1, 1), 2.8, "LDPR", OrganisationType.GOVERNMENT, new Address("ул. Ленина", new Location(1.0, 1F, "Санкт-Петербург")));
 
         System.out.println(org1.getId());
         System.out.println(org2.getId());
--- programming1/5lab/src/commands/Help.java
+++ programming1/5lab/src/commands/Help.java
@@ -1,16 +1,24 @@
 package src.commands;
 
+import java.util.ArrayList;
+
 public class Help extends Command{
     private String name;
     private String description;
+    private ArrayList<Command> listOfCommands;
 
-    public Help(String aName, String aDescription) {
+    public Help(String aName, String aDescription, ArrayList<Command> aListOfCommands) {
         super(aName, aDescription);
+        listOfCommands = aListOfCommands;
     }
 
     @Override
     public String execute() {
-        return "";
+        String strListOfCommands = this.toString() + "\n";
+        for (Command command : listOfCommands ) {
+            strListOfCommands += command.toString() + "\n";
+        }
+        return strListOfCommands;
     }
 
     public String getName() {
