36d18d5847eef7e9ed99a3256e81dd6b55ca3028
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-02-17 20:47:25 +0300

    Added CommandType to make arrangement of commands

--- programming1/5lab/src/commands/classes/CommandController.java
+++ programming1/5lab/src/commands/classes/CommandController.java
@@ -9,6 +9,7 @@ import src.data.classes.CollectionWorker;
 
 public class CommandController implements Executable {
     private HashMap<String, Command> mapOfCommands;
+    private boolean notEnded;
 
     public CommandController() {
         mapOfCommands = new HashMap<>();
@@ -20,16 +21,25 @@ public class CommandController implements Executable {
         mapOfCommands.put(insertCommand.getName(), insertCommand);
         Exit exitCommand = new Exit();
         mapOfCommands.put(exitCommand.getName(), exitCommand);
+        notEnded = true;
     }
 
     public Map<String, Command> getMapOfCommands() {
         return mapOfCommands;
     }
 
+    public boolean isNotEnded() {
+        return notEnded;
+    }
+
+    public void setNotEnded(boolean aNotEnded) {
+        this.notEnded = aNotEnded;
+    }
+
     @Override
     public String execute(CollectionWorker worker, String nameOfCommand, ArrayList<String> extraArguments) {
         StringBuilder execution = new StringBuilder();
-        if (mapOfCommands.containsKey(nameOfCommand)) {
+        if (mapOfCommands.containsKey(nameOfCommand) && notEnded) {
             execution.append(mapOfCommands.get(nameOfCommand).execute(worker, nameOfCommand, extraArguments) + "\n");// Adding into String Builder argument.execute()
         } else {
             execution.append("There is no command with that name. Try again.\n"); // return "There is no comand with that name. Try again."
