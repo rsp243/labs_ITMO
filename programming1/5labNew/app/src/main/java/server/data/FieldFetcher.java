be0d47678e2f6f494f6cefc6aec6af6cbc9a2134
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-03-25 20:04:14 +0300

    new recursive FieldFetcher

--- programming1/5labNew/app/src/main/java/server/data/FieldFetcher.java
+++ programming1/5labNew/app/src/main/java/server/data/FieldFetcher.java
@@ -5,42 +5,47 @@ import java.util.LinkedHashMap;
 
 import server.data.Annotations.Complex;
 
-/**
- * FieldFetcher Class
- * return LinkedHashMap of declared Fields of main Class 
- */
-
 public class FieldFetcher {
     /**
      * fetchFields method
-     * @return LinkedHashMap of declared Fields of main Class 
+     *
+     * @return LinkedHashMap of declared Fields of main Class
      */
     public LinkedHashMap<String, String> fetchFields() {
         var resultHashMap = new LinkedHashMap<String, String>();
         Field[] cityClass = City.class.getDeclaredFields();
         for (Field cityField : cityClass) {
             if (cityField.isAnnotationPresent(Complex.class)) {
-                Field[] fieldsOfCityField = cityField.getType().getDeclaredFields();
-                StringBuilder enumValues = new StringBuilder();
-                for (Field field : fieldsOfCityField) {
-                    if (!cityField.getType().isEnum()) {
-                        resultHashMap.put(cityField.getDeclaringClass().getSimpleName() + "."
-                                + cityField.getType().getSimpleName() + "." + field.getName(),
-                                field.getType().getSimpleName());
-                    } else {
-                        if (field.getName() != "$VALUES") {
-                            enumValues.append(field.getName() + ", ");
-                        } else {
-                            resultHashMap.put(cityField.getDeclaringClass().getSimpleName() + "."
-                                    + cityField.getType().getSimpleName(),
-                                    "Enum. Values: " + enumValues.delete(enumValues.length() - 2, enumValues.length())
-                                            .toString());
-                        }
-                    }
-                }
+                resultHashMap.putAll(this.fetchFromField(cityField, "StudyGroup", new StringBuilder()));
             } else {
                 resultHashMap.put(cityField.getDeclaringClass().getSimpleName() + "." + cityField.getName(),
                         cityField.getType().getSimpleName());
+            }
+        }
+        return resultHashMap;
+    }
+
+    public LinkedHashMap<String, String> fetchFromField(Field fetchField, String keyValue, StringBuilder enumValues) {
+        var resultHashMap = new LinkedHashMap<String, String>();
+        Field[] fetchedClass = fetchField.getType().getDeclaredFields();
+        keyValue += "." + fetchField.getType().getSimpleName();
+        for (Field fetchedFieldInClass : fetchedClass) {
+            if (fetchedFieldInClass.isAnnotationPresent(Complex.class)) {
+                resultHashMap.putAll(this.fetchFromField(fetchedFieldInClass, keyValue, enumValues));
+            } else {
+                if (fetchedFieldInClass.getType().isEnum()) {
+                    enumValues.append(fetchedFieldInClass.getName() + ", ");
+                } else {
+                    if (fetchedFieldInClass.getName().equals("ENUM$VALUES")
+                            || fetchedFieldInClass.getName().equals("$VALUES")) {
+                        resultHashMap.put(keyValue,
+                                "Enum. Values: "
+                                        + enumValues.delete(enumValues.length() - 2, enumValues.length()).toString());
+                    } else {
+                        resultHashMap.put(keyValue + "." + fetchedFieldInClass.getName(),
+                                fetchedFieldInClass.getType().getSimpleName());
+                    }
+                }
             }
         }
         return resultHashMap;
