27b441de476c6d8e03327fd2dafc946005ae1f9e
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-10-02 22:48:53 +0300

    refactor: add a little handler of errors to AreaCheckServlet

--- WEB/2lab/app/src/main/java/servlets/AreaCheckServlet.java
+++ WEB/2lab/app/src/main/java/servlets/AreaCheckServlet.java
@@ -27,6 +27,53 @@ public class AreaCheckServlet extends HttpServlet {
         resp.setContentType("text/html");
         String queryString = req.getQueryString();
         PrintWriter out = resp.getWriter();
+        String contextPath = getServletContext().getContextPath();
+        out.println(
+                """
+                        <!doctype html>
+                        <head>
+                            <meta charset="utf-8">
+                            <title>Reshetov Semen P3206 WEB 2lab</title>
+                            <meta name="description" content="Reshetov Semen P3206 WEB 2lab">
+                            <meta name="author" content="Reshetov Semen">
+                            <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <link rel="preconnect" href="https://fonts.googleapis.com">
+                            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                            <link
+                                href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&family=Poppins:wght@400;500;600;700&display=swap"
+                                rel="stylesheet">
+                                """);
+        out.println("<link rel=\"stylesheet\" href=\"" + contextPath
+                + "/src/css/areaCheckStyles.css\"></head>");
+        out.println(
+                """
+                            <body>
+                                <header>
+                                    <div class="introduction">
+                                        <h3>First web laboratory</h3>
+                                        <strong>Reshetov Semen, P3206. Variant: 1623</strong>
+                                    </div>
+                                    <div class="links">
+                                        <form action="https://se.ifmo.ru/courses/web" target="_blank">
+                                            <button class="nav-button">
+                                                SE VITMO
+                                            </button>
+                                        </form>
+                                        <form action="https://github.com/rsp243" target="_blank">
+                                            <button class="nav-button">
+                                                Github account
+                                            </button>
+                                        </form>
+                                        <form action="https://t.me/rsp243" target="_blank">
+                                            <button class="nav-button">
+                                                Telegram account
+                                            </button>
+                                        </form>
+                                    </div>
+                                </header>
+                                <div class="main-container">
+
+                        """);
         if (queryString != null) {
             long nowTime = System.nanoTime();
             String[] parametersPairs = queryString.split("&");
@@ -70,56 +117,9 @@ public class AreaCheckServlet extends HttpServlet {
                 UserData userData = new UserData(xVal, yVal, rVal, isHit, currentTime, executionTime.toString(),
                         pointColor);
                 userDataList.getUserDataList().add(userData);
-                String contextPath = getServletContext().getContextPath();
 
                 out.println(
                         """
-                                <!doctype html>
-                                <head>
-                                    <meta charset="utf-8">
-                                    <title>Reshetov Semen P3206 WEB 2lab</title>
-                                    <meta name="description" content="Reshetov Semen P3206 WEB 2lab">
-                                    <meta name="author" content="Reshetov Semen">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                                    <link rel="preconnect" href="https://fonts.googleapis.com">
-                                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                                    <link
-                                        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&family=Poppins:wght@400;500;600;700&display=swap"
-                                        rel="stylesheet">
-                                        """);
-                out.println("<link rel=\"stylesheet\" href=\"" + contextPath
-                        + "/src/css/areaCheckStyles.css\"></head>");
-                out.println(
-                        """
-                                    <body>
-                                        <header>
-                                            <div class="introduction">
-                                                <h3>First web laboratory</h3>
-                                                <strong>Reshetov Semen, P3206. Variant: 1623</strong>
-                                            </div>
-                                            <div class="links">
-                                                <form action="https://se.ifmo.ru/courses/web" target="_blank">
-                                                    <button class="nav-button">
-                                                        SE VITMO
-                                                    </button>
-                                                </form>
-                                                <form action="https://github.com/rsp243" target="_blank">
-                                                    <button class="nav-button">
-                                                        Github account
-                                                    </button>
-                                                </form>
-                                                <form action="https://t.me/rsp243" target="_blank">
-                                                    <button class="nav-button">
-                                                        Telegram account
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        </header>
-
-                                """);
-                out.println(
-                        """
-                                <div class="main-container">
                                     <div class="data-table">
                                         <table cellspacing="1" cellpadding="10" width="100%">
                                             <thead>
@@ -146,20 +146,19 @@ public class AreaCheckServlet extends HttpServlet {
                                             </tbody>
                                             </table>
                                         </div>
-                                        <div class="btn-block">
                         """);
-                out.println("<a href=\"" + contextPath
-                        + "\"><button class=\"control-btn\">Back To The Main Page</button></a>");
-                out.println("""
-                                    </div>
-
-                                </div>
-                        """);
-                out.println("""
-                        </body>
-                        </html>
-                            """);
             }
         }
+        out.println("<div class=\"btn-block\">");
+        out.println("<a href=\"" + contextPath
+                + "\"><button class=\"control-btn\">Back To The Main Page</button></a>");
+        out.println("""
+                            </div>
+                        </div>
+                """);
+        out.println("""
+                </body>
+                </html>
+                    """);
     }
 }
--- WEB/2lab/app/src/main/java/servlets/ControllerServlet.java
+++ WEB/2lab/app/src/main/java/servlets/ControllerServlet.java
@@ -3,5 +3,5 @@ package servlets;
 import jakarta.servlet.http.HttpServlet;
 
 public class ControllerServlet extends HttpServlet {
-
+    
 }
