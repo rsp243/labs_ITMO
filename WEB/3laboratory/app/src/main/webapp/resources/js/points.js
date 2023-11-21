e9fbd6d0c0719c01b1b0419592ed83d3415dbd22
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-11-21 04:16:29 +0300

    refactor(3laboratory): refactor all animation functions

--- WEB/3laboratory/app/src/main/webapp/resources/js/points.js
+++ WEB/3laboratory/app/src/main/webapp/resources/js/points.js
@@ -1,24 +1,5 @@
-let canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
-
-let canvasWithAxes = document.getElementById("canvasWithAxes"),
-    ctxWithAxes = canvasWithAxes.getContext('2d');
-
-class Point {
-    constructor(x, y, r, imgSrc, offsetX, offsetY, scale, rotationAngle) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.imgSrc = imgSrc;
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
-        this.scale = scale;
-        this.rotationAngle = rotationAngle;
-    }
-}
-
-let pointArray = []
-
+let canvas1 = document.getElementById("canvas")
+ctx1 = canvas1.getContext('2d');
 
 // function drawPersistantRotatedPoint(xValue, yValue, rValue, img, offsetX, offsetY, scale, rotationAngle, ctx1) {
 //     let xOnCanvas = canvas.width / 2 + canvas.width / 3 * (xValue / rValue)
@@ -33,103 +14,90 @@ let pointArray = []
 //     ctx1.save()
 // }
 
-function drawAnimatedRotatedPoint(xValue, yValue, rValue, img, offsetX, offsetY, scale, rotationAngle, ctx) {
-    let xOnCanvas = canvas.width / 2 + canvas.width / 3 * (xValue / rValue)
-    let yOnCanvas = canvas.width / 2 - canvas.width / 3 * (yValue / rValue)
+function drawAnimatedRotatedPoint(xValue, yValue, rValue, imgHeight, imageSrc, offsetX, offsetY, rotationAngle, ctx) {
+    let img = new Image()
+    img.onload = function () {
+        ctx1.clearRect(0, 0, canvas.width, canvas.height);
+        let xOnCanvas = canvas.width / 2 + canvas.width / 3 * (xValue / rValue)
+        let yOnCanvas = canvas.width / 2 - canvas.width / 3 * (yValue / rValue)
 
-    // let xOnCanvasOff = canvas.width / 2 + canvas.width / 3 * (xValue / rValue) + offsetX
-    // let yOnCanvasOff = canvas.width / 2 - canvas.width / 3 * (yValue / rValue) + offsetY
+        let imageScale = imgHeight / img.height
+        ctx.save()
+        ctx.translate(xOnCanvas, yOnCanvas); // move to point
 
-    // let rotation = degrees * Math.PI / 180
+        ctx.setTransform(imageScale, 0, 0, imageScale, xOnCanvas, yOnCanvas); // sets scale and origin
 
-    ctx.save()
-    // ctx.clearRect(0, 0, 300, 300); // clear canvas
-    ctx.translate(xOnCanvas, yOnCanvas); // move to point
+        ctx.rotate(rotationAngle);
+        ctx.drawImage(img, -img.width / 2 - offsetX, -img.height / 2 - offsetY);
+        ctx.restore()
+    };
+    img.src = imageSrc
+}
 
-    ctx.setTransform(scale, 0, 0, scale, xOnCanvas, yOnCanvas); // sets scale and origin
+function drawPoint(xValue, yValue, rValue, isHit, imageSrc, rotationAngle, canvasIDstr, isAxeAnimated) {
+    let axeHeight = 40 // need to fix
+    let fissureHeight = 25 // need to fix
 
-    ctx.rotate(rotationAngle);
-    ctx.drawImage(img, -img.width / 2 - offsetX, -img.height / 2 - offsetY);
-    ctx.restore()
+    let canvas = document.getElementById(canvasIDstr),
+        ctx = canvas.getContext('2d');
+
+    if (isAxeAnimated) {
+        drawAnimatedRotatedPoint(xValue, yValue, rValue, axeHeight, imageSrc, axeHeight * 5, - axeHeight, rotationAngle, ctx)
+    } else {
+        drawAnimatedRotatedPoint(xValue, yValue, rValue, fissureHeight, imageSrc, 0, 0, rotationAngle, ctx)
+    }
 }
 
 function drawIsHitPoint(xValue, yValue, rValue, isHit) {
-    let xValueRotate = xValue - rValue * 3
-
-    let axeImg = new Image()
-    let axeHeight = 40
-
-    let fissureImg = new Image()
-    let fissureHeight = 25
-    let fissureScale = fissureHeight / fissureImg.height
-
-    fissureScale = 0.1
+    let rValueScale = 3
+    let xValueRotate = xValue - rValue * rValueScale
 
     let frameCount = 80
     let frameRestCount = frameCount
     let angle = 0
-    let axeScale
     let rotation
+
 
     setTimeout(drawFhrow)
 
     function drawFhrow() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         angle = (Math.random() * (23 - 2) + 2 + angle) % 360
-        axeScale = axeHeight / axeImg.height
         rotation = angle * Math.PI / 180
-        axeImg.onload = function () {
-            drawAnimatedRotatedPoint(xValueRotate, yValue, rValue, axeImg, axeHeight * 5, - axeHeight, axeScale, rotation, ctx)
-        };
-        axeImg.src = axeImageSrc
+
+        drawPoint(xValueRotate, yValue, rValue, isHit, axeImageSrc, rotation, "canvas", true)
 
         frameRestCount--
-        xValueRotate = xValueRotate + rValue / frameCount * 3
+        xValueRotate = xValueRotate + rValue / frameCount * rValueScale
         if (frameRestCount != 0) {
             setTimeout(drawFhrow, 10)
         } else {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            fissureImg.onload = function () {
-                drawAnimatedRotatedPoint(xValue, yValue, rValue, fissureImg, 0, 0, fissureScale, rotation, ctxWithAxes)
-                drawIsHit()
-            };
-            fissureImg.src = fissureImageSrc
+            drawPoint(xValue, yValue, rValue, isHit, fissureImageSrc, rotation, "canvasWithAxes", false)
+            drawIsHit()
         }
     }
 
     function drawIsHit() {
+        let yValueRotate = yValue
         if (isHit) {
-            axeImg.onload = function () {
-                drawAnimatedRotatedPoint(xValue, yValue, rValue, axeImg, axeHeight * 5, - axeHeight, axeScale, rotation, ctxWithAxes)
-                pointArray.push(new Point(xValue, yValue, rValue, axeImageSrc, axeHeight * 5, - axeHeight, axeScale, rotation))
-            };
-            axeImg.src = axeImageSrc
+            drawPoint(xValueRotate, yValue, rValue, isHit, axeImageSrc, rotation, "canvasWithAxes", true)
         } else {
             frameRestCount = frameCount
             setTimeout(drawNotHit)
         }
-
-        let yValueRotate = yValue
-
         function drawNotHit() {
-            xValueRotate -= rValue / frameCount * 3 / 2
-            yValueRotate -= ((xValue - xValueRotate)  ** 2) / frameCount * 4
+            xValueRotate -= rValue / frameCount * rValueScale / 2
+            yValueRotate -= ((xValue - xValueRotate) ** 2) / frameCount * 4
 
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
             angle = (Math.random() * (23 - 2) + 2 + angle) % 360
-            let axeScale = axeHeight / axeImg.height
             let rotation = angle * Math.PI / 180
-            axeImg.onload = function () {
-                drawAnimatedRotatedPoint(xValueRotate, yValueRotate, rValue, axeImg, axeHeight * 5, - axeHeight, axeScale, - rotation, ctx)
-            };
-            axeImg.src = axeImageSrc
-    
+            drawPoint(xValueRotate, yValueRotate, rValue, isHit, axeImageSrc, rotation, "canvas", true)
+
             frameRestCount--
             if (frameRestCount != 0) {
                 setTimeout(drawNotHit, 10)
             }
         }
+
     }
 }
