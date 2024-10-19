3ad0321c9ecdfb93b136dd8a7b3da8c69ffbfc13
rsp243 <reshetov.semjon@gmail.com>
2024-10-19 16:59:42 +0300

    feat(IS/1lab/frontend): perform main page, add new edit&delete

--- IS/1lab/frontend/src/components/App/ResultTable.jsx
+++ IS/1lab/frontend/src/components/App/ResultTable.jsx
@@ -7,6 +7,8 @@ import { Messages } from 'primereact/messages';
 import { TabMenu } from 'primereact/tabmenu';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
 
 
 export default function ResultTable({getToken}) {
@@ -24,18 +26,22 @@ export default function ResultTable({getToken}) {
     const [expandedRows, setExpandedRows] = useState({});
 
 
-    function performCoordinatesOption(coord)  {
-        return {
-            label: `id: ${coord.id}, X: ${coord.x}, Y: ${coord.y}`, // Constructing the label
-            value: `${coord.id}` // Constructing the value
-        };
+    function performCoordinatesOptions(coords)  {
+        return coords.map(coord => {
+            return {
+                label: `id: ${coord.id}, X: ${coord.x}, Y: ${coord.y}`, // Constructing the label
+                value: `${coord.id}` // Constructing the value
+            };
+        });
     }
     
-    function performLocationOption(loc)  {
-        return {
-            label: `id: ${loc.id}, X: ${loc.x}, Y: ${loc.y}, Z: ${loc.z}`, // Constructing the label
-            value: `${loc.id}` // Constructing the value
-        };
+    function performLocationOptions(locations)  {
+        return locations.map(loc => {
+            return {
+                label: `id: ${loc.id}, X: ${loc.x}, Y: ${loc.y}, Z: ${loc.z}`, // Constructing the label
+                value: `${loc.id}` // Constructing the value
+            };
+        });
     }
 
     const handleEditClick = (id) => {
@@ -56,10 +62,74 @@ export default function ResultTable({getToken}) {
         axios.post(`http://localhost:8080/api/v1/person/delete`, data)
             .then(res => {
                 console.log(res)
-                setReload(!reload)
                 msgs.current.show([
                     { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: res.data.message, closable: false },
                 ])
+                deleteRow(id)
+            })
+            .catch(function (error) {
+                let myError = "";
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    
+                    myError = error.response.data.message
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                    myError = "An error during request setting up has happened"
+                }
+                msgs.current.replace([
+                    { severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+                ]);
+            });
+    }
+
+    const handleDeleteClickCoordinates = async (id) => {
+        let data = {
+            id: id,
+            token: getToken()
+        }
+        msgs.current.clear();
+        axios.post(`http://localhost:8080/api/v1/coordinates/delete`, data)
+            .then(res => {
+                console.log(res)
+                msgs.current.show([
+                    { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: res.data.message, closable: false },
+                ])
+                deleteRowCoordinates(id)
+            })
+            .catch(function (error) {
+                let myError = "";
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    
+                    myError = error.response.data.message
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                    myError = "An error during request setting up has happened"
+                }
+                msgs.current.replace([
+                    { severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+                ]);
+            });
+    }
+
+    const handleDeleteClickLocation = async (id) => {
+        let data = {
+            id: id,
+            token: getToken()
+        }
+        msgs.current.clear();
+        axios.post(`http://localhost:8080/api/v1/location/delete`, data)
+            .then(res => {
+                console.log(res)
+                msgs.current.show([
+                    { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: res.data.message, closable: false },
+                ])
+                deleteRowLocation(id)
             })
             .catch(function (error) {
                 let myError = "";
@@ -186,44 +256,256 @@ export default function ResultTable({getToken}) {
         },
     ]
 
-    const users = [
-        { id: 1, name: 'John Doe', email: 'john@example.com' },
-        { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice@example.com' }
-    ];
+    const onRowEditComplete = (e) => {
+        let _results = [...results];
+        let { newData, index } = e;
+
+        _results[index] = newData;
+
+        const personData = {
+            id: newData.id,
+            name: newData.name,
+            coordinates_id: newData.coordinates_id,
+            eye_color: newData.eyeColor,
+            hair_color: newData.hairColor,
+            location_id: newData.location_id,
+            height: newData.height,
+            nationality: newData.nationality,
+            token: getToken()
+        };
+
+        msgs.current.clear();
+        axios.post(`http://localhost:8080/api/v1/person/edit`, personData)
+            .then(res => {
+                console.log(res.data);
+                msgs.current.show([
+                    { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: 'Successfully edited', closable: false },
+                ])
+            })
+            .catch(function (error) {
+                let myError = error.code;
+                if (error.response) {
+                    myError += " " + error.response.code;
+                } else {
+                    myError = "An error during request setting up has happened"
+                }
+                msgs.current.show([
+                    { severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+                ]);
+            });
+        setResults(_results);
+    };
+
+    const textEditor = (options) => {
+        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+    };
+
+    const numEditor = (options) => {
+        return <InputText type="number" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />
+    };
+
+    const colorOptions = [
+        { label: 'RED', value: 'RED' },
+        { label: 'GREEN', value: 'GREEN' },
+        { label: 'BROWN', value: 'BROWN' }
+    ]
+
+    const countryOptions = [
+        {label: "RUSSIA", value: "RUSSIA"},
+        {label: "VATICAN", value: "VATICAN"},
+        {label: "SOUTH_KOREA", value: "SOUTH_KOREA"},
+        {label: "JAPAN", value: "JAPAN"}
+    ]
+
+    const coordinatesEditor = (options) => {
+        return (
+            <Dropdown
+                value={String(options.value)}
+                options={performCoordinatesOptions(resultsCoordinates)}
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select Coordinates"
+            />
+        );
+    };
+
+    const locationEditor = (options) => {
+        return (
+            <Dropdown
+                value={String(options.value)}
+                options={performLocationOptions(resultsLocation)}
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select Location"
+            />
+        );
+    };
+
+    const nationalityEditor = (options) => {
+        return (
+            <Dropdown 
+                options={countryOptions} 
+                value={options.value}
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select Nationality" 
+            />
+        )
+    }
+    const colorEditor = (options) => {
+        return (
+            <Dropdown 
+                options={colorOptions} 
+                value={options.value}
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select Color"
+            />
+        )
+    }
+
+    const onRowEditCompleteCoordinates = (e) => {
+        let _results = [...resultsCoordinates];
+        let { newData, index } = e;
+
+        _results[index] = newData;
+
+        const coordinatesData = {
+            id: newData.id,
+            x: newData.x,
+            y: newData.y,
+            token: getToken()
+        };
+
+        msgs.current.clear();
+        axios.post(`http://localhost:8080/api/v1/coordinates/edit`, coordinatesData)
+            .then(res => {
+                console.log(res.data);
+                msgs.current.show([
+                    { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: 'Successfully edited', closable: false },
+                ])
+            })
+            .catch(function (error) {
+                let myError = error.code;
+                if (error.response) {
+                    myError += " " + error.response.code;
+                } else {
+                    myError = "An error during request setting up has happened"
+                }
+                msgs.current.show([
+                    { severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+                ]);
+            });
+        setResultsCoordinates(_results);
+    };
+
+    const onRowEditCompleteLocation = (e) => {
+        let _results = [...resultsLocation];
+        let { newData, index } = e;
+
+        _results[index] = newData;
+
+        const locationData = {
+            id: newData.id,
+            x: newData.x,
+            y: newData.y,
+            z: newData.z,
+            token: getToken()
+        };
+
+        msgs.current.clear();
+        axios.post(`http://localhost:8080/api/v1/location/edit`, locationData)
+            .then(res => {
+                console.log(res.data);
+                msgs.current.show([
+                    { sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: 'Successfully edited', closable: false },
+                ])
+            })
+            .catch(function (error) {
+                let myError = error.code;
+                if (error.response) {
+                    myError += " " + error.response.code;
+                } else {
+                    myError = "An error during request setting up has happened"
+                }
+                msgs.current.show([
+                    { severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+                ]);
+            });
+        setResultsLocation(_results);
+    };
+
+    const deleteRow = (id) => {
+        setResults((prevResults) => prevResults.filter((result) => result.id !== id));
+    };
+
+    const deleteRowCoordinates = (id) => {
+        setResultsCoordinates((prevResults) => prevResults.filter((result) => result.id !== id));
+    };
+
+    const deleteRowLocation = (id) => {
+        setResultsLocation((prevResults) => prevResults.filter((result) => result.id !== id));
+    };
+
+    const deleteTemplate = (rowData) => {
+        return (
+            <Button icon="pi pi-trash" className="p-button-danger" onClick={() => handleDeleteClick(rowData.id)} />
+        );
+    };
+
+    const deleteTemplateCoordinates = (rowData) => {
+        return (
+            <Button icon="pi pi-trash" className="p-button-danger" onClick={() => handleDeleteClickCoordinates(rowData.id)} />
+        );
+    };
+
+    const deleteTemplateLocation = (rowData) => {
+        return (
+            <Button icon="pi pi-trash" className="p-button-danger" onClick={() => handleDeleteClickLocation(rowData.id)} />
+        );
+    }; 
+
+    const allowEdit = (rowData) => {
+        return rowData.name !== 'Blue Band';
+    };
+
     return (
         <>
             <div className="card flex flex-column justify-content-center align-items-center">
                 <Messages ref={msgs} />
             </div>
-            <div className="" style={{ width: '80%', borderCollapse: 'collapse', margin: "auto auto" }}>
+            <div className="card" style={{ width: '80%', borderCollapse: 'collapse', margin: "auto auto", backgroudColor: "#343e4d"}}>
                 <TabMenu 
-                    model={tableItems} 
+                    model={tableItems}
+                    activeIndex={tableVal}
                     onTabChange={handleTabChange} />
                 { tableVal === 0 &&
-                    (<DataTable value={results} paginator rows={5} header="Person List">
-                        <Column field="id" header="ID" />
-                        <Column field="name" header="Name" />
-                        <Column field="coordinates_id" header="Coordinates" />
-                        <Column field="eyeColor" header="Eye Color" />
-                        <Column field="hairColor" header="Hair Color" />
-                        <Column field="location_id" header="Location" />
-                        <Column field="height" header="Height (cm)" />
-                        <Column field="nationality" header="Nationality" />
+                    (<DataTable value={results} removableSort sortMode="multiple" editMode="row" dataKey="id" onRowEditComplete={onRowEditComplete} showGridlines paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} header="Person List">
+                        <Column field="id" header="ID" sortable />
+                        <Column editor={(options) => textEditor(options)} field="name" header="Name" sortable />
+                        <Column editor={(options) => coordinatesEditor(options)} field="coordinates_id" header="Coordinates ID" sortable />
+                        <Column editor={(options) => colorEditor(options)} field="eyeColor" header="Eye Color" sortable />
+                        <Column editor={(options) => colorEditor(options)} field="hairColor" header="Hair Color" sortable />
+                        <Column editor={(options) => locationEditor(options)} field="location_id" header="Location ID" sortable />
+                        <Column editor={(options) => numEditor(options)} field="height" header="Height (cm)" sortable />
+                        <Column editor={(options) => nationalityEditor(options)} field="nationality" header="Nationality" sortable />
+                        <Column rowEditor={allowEdit} header="Edit"/>
+                        <Column body={deleteTemplate} header="Delete" />
                     </DataTable>)
                 }
                 { tableVal === 1 &&
-                    (<DataTable value={users} paginator rows={5} header="Coordinates List">
-                        <Column field="id" header="ID" />
-                        <Column field="name" header="Name" />
-                        <Column field="email" header="Email" />
+                    (<DataTable value={resultsCoordinates} removableSort sortMode="multiple" editMode="row" dataKey="id" onRowEditComplete={onRowEditCompleteCoordinates} showGridlines paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} header="Coordinates List">
+                        <Column field="id" header="ID" sortable />
+                        <Column editor={(options) => numEditor(options)} field="x" header="X" sortable />
+                        <Column editor={(options) => numEditor(options)} field="y" header="Y" sortable />
+                        <Column rowEditor={allowEdit} header="Edit" />
+                        <Column body={deleteTemplateCoordinates} header="Delete" />
                     </DataTable>)
                 }
                 { tableVal === 2 &&
-                    (<DataTable value={users} paginator rows={5} header="Location List">
-                        <Column field="id" header="ID" />
-                        <Column field="name" header="Name" />
-                        <Column field="email" header="Email" />
+                    (<DataTable value={resultsLocation} removableSort sortMode="multiple" editMode="row" dataKey="id" onRowEditComplete={onRowEditCompleteLocation} showGridlines paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} header="Location List">
+                        <Column field="id" header="ID" sortable/>
+                        <Column editor={(options) => numEditor(options)} field="x" header="X" sortable/>
+                        <Column editor={(options) => numEditor(options)} field="y" header="Y" sortable/>
+                        <Column editor={(options) => numEditor(options)} field="z" header="Z" sortable/>
+                        <Column rowEditor={allowEdit} header="Edit" />
+                        <Column body={deleteTemplateLocation} header="Delete" />
                     </DataTable>)
                 }
                 {/* <table>
